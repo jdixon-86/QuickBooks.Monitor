cd3cc22b687d084e47241ddebca6f48655b3d85f
Kaleb Lape <pingortle@gmail.com>
2014-07-24 15:09:37 -0500

    Refactor settings.

---
+++ QuickBooks Monitor/QBMonitorSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace QuickBooks_Monitor
+{
+    public class QBMonitorSettings
+    {
+        public QBMonitorSettings(XDocument doc)
+        {
+            _paths = doc.Element("watcher").Elements("data").Select(e =>
+                {
+                    return new QuickBooksPaths
+                        {
+                            Path = e.Element("path").Value,
+                            Filename = e.Element("filename").Value,
+                            LookFor = e.Element("line").Value,
+                            ReplaceWith = e.Element("replace").Value,
+                        };
+                })
+                .ToList();
+        }
+
+        private readonly IEnumerable<QuickBooksPaths> _paths;
+    }
+}
